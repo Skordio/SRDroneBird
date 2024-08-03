360190e3523eb5b422dc7a00a38e8e2395914760
Skordio <skordion@hotmail.com>
2024-08-02 21:32:06 -0400

    change description to be more professional

--- src/pages/about/page.vue
+++ src/pages/about/page.vue
@@ -12,19 +12,12 @@
                         Hi, my name is Steven Wells.
                     </p>
                     <p>
-                        SR DroneBird is a drone photography and videography company I am trying to start - based near Scranton, Pennsylvania. In fact, I am currently in the process of designing this website for the company!
+                        SR DroneBird is my drone photography and videography company based in the Pocono Mountains, half an hour from Scranton.
                     </p>
                     <p>
-                        I started flying drones about two years ago as just a personal hobby, and in the time since then it has become one of my favorite things to do.
-                        I love to fly my drones so much that I want to offer my services to anyone looking for a drone photographer or videographer, for any type of personal project or event.
-                        So, if you are looking for someone who can take drone footage, I would love to hear from you! There will soon be a contact section of this website where you can reach out to me.
-                    </p>
-                    <!-- <p>
-                        I have been flying drones for almost a year now as a personal hobby, and I am now looking to offer
-                        my services as
-                        a drone photographer. I am willing to do any job, ranging from weddings to real estate to personal
-                        projects.
-                    </p> -->
+                        I have been flying drones for three years as a personal hobby, and I am now looking to offer
+                        my services as a drone videographer.
+                    </p> 
                     <p>
                         If you would like to check out some of my flying, head over to the gallery section of this website -
                         there you can find several edited videos and short clips I've taken near Lake Wallenpaupack in the Pocono Mountains of Pennsylvania.
@@ -43,24 +36,21 @@
                             DJI Avata
                         </li>
                         <li>
-                            DJI FPV
-                        </li>
-                        <li>
-                            iFlight Nazgul V3
+                            iFlight Nazgul V3 HD
                         </li>
                     </ul>
                 </v-card>
             </v-col>
         </v-row>
-        <v-row>
+        <!-- <v-row>
             <v-col cols="6">
                 <v-card class="about-card pa-3" elevation="10">
                     <p>
-                        Note: I am currently in the process of obtaining my commercial drone license; I expect to have it around mid-spring 2024.  
+                        Note: I am currently in the process of obtaining my commercial drone license; I expect to have it around last-quarter 2024.  
                     </p>
                 </v-card>
             </v-col>
-        </v-row>
+        </v-row> -->
     </v-container>
 </template>
 
