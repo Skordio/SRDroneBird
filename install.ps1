51a4846cc12332d2649c2f7a16094ee8f5ca1d5b
Skordio <skordion@hotmail.com>
2025-02-24 15:57:50 -0500

    update pnpm install script

--- install.ps1
+++
@@ -1,4 +0,0 @@
-iwr https://get.pnpm.io/install.ps1 -useb | iex;
-git pull;
-pnpm install -r;
-./downloadAssets.ps1;
--- pnpm_install.ps1
+++ pnpm_install.ps1
@@ -1,4 +1,3 @@
 iwr https://get.pnpm.io/install.ps1 -useb | iex;
 git pull;
 pnpm install -r;
-pnpm run dev;
